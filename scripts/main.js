966cf3ce96faa4b81e533cb0449073407c766250
Brittany Rollins <artemismoon2015@gmail.com>
2016-07-20 14:39:42 -0400

    initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,51 +1,34 @@
-$(document).ready(function(){
-	setInterval(function(){
-	var dateTime = new Date();
-	var minutesTwoDigitsWithLeadingZero = ("0" + dateTime.getMinutes()).substr(-2);
-	$('#milliseconds').text(dateTime.getMilliseconds());
-	$('#seconds').text(dateTime.getSeconds());
-	$('#minutes').text(minutesTwoDigitsWithLeadingZero);
-	$('#hours').text(dateTime.getHours());
 
-		if (minutesTwoDigitsWithLeadingZero % 2 === 0){
-		$('body').css({"background-color":"blue"});
-		} else if (minutesTwoDigitsWithLeadingZero % 2 !== 0){
-		$('body').css({"background-color":"black"});}
+	/* The Modal (background) */
 
-		var colors = ['#ff0000', '#00ff00', '#0000ff'];
-		if (minutesTwoDigitsWithLeadingZero % 5 === 0){
-		var randcolors = colors[Math.floor(Math.random() * colors.length)];
-		$('body').css('background-color', randcolors)};
+// Get the modal
+var modal = document.getElementById('myModal');
 
-		if (minutesTwoDigitsWithLeadingZero % 60 === 0){
-		var randcolors = colors[Math.floor(Math.random() * colors.length)];
-		$('#clock').css('color', randcolors)};
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-	}, 100)
-});
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
 		
 	
 	
-
-
-		/*Color[] colors;
-
-		colors = new Color[4];
-		colors[0] = new Color(Color.red);
-		colors[1] = new Color(Color.blue);
-		colors[2] = new Color(Color.purple);
-		colors[3] = new Color(Color.white);
-
-
-	function multfive(#minutes){
-		return (#minutes%5 == 0);
-	}
-
-		if(multfive(#minutes)){
-			background-color: 'Color'
-		}*/
-
-
 
 
 
